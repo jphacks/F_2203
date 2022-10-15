674ef200dbc6f057121e12e54a3996340455cfc2
tatara-takumi <tataratakumi@gmail.com>
2022-10-15 14:34:38 +0900

    'ログイン機能'

--- packages/frontend/src/pages/login.tsx
+++ packages/frontend/src/pages/login.tsx
@@ -1,6 +1,30 @@
-import React from "react"
+import React, { FC, useEffect, useReducer } from "react"
+import authReducer from "@/reducers/authReducer"
+import { useAuthUser } from "@/hooks/useAuth"
+import { useRouter } from "next/router"
+import { authUseCase } from "@/useCases"
 
-const Login = () => {
+const Login: FC = () => {
+    const router = useRouter()
+    const [, dispatch] = useReducer(authReducer.reducer, authReducer.initialState)
+    const user = useAuthUser()
+
+    useEffect(() => {
+        console.log(user)
+        if (user != null && !user.isAnonymous) {
+            router.push("/")
+        }
+    }, [user])
+
+    const logIn = async () => {
+        try {
+            await authUseCase.signIn(dispatch)
+            router.push("/")
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <main className="bg-[#001B36] mx-auto my-auto min-h-screen justify-center flex items-center">
             <div>
@@ -13,6 +37,7 @@ const Login = () => {
                 <div className="justify-center flex">
                     <button
                         className="border rounded-lg py-2 px-4"
+                        onClick={logIn}
                     >
                         <p className="font-medium text-white" >
                             Googleアカウントでログイン
