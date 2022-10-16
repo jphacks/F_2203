029cb3fe4cc5255014cdf5bb58e9baa1dbea0835
putcho01 <>
2022-10-16 16:34:25 +0900

    Fix: conflict

--- packages/frontend/src/pages/login.tsx
+++ packages/frontend/src/pages/login.tsx
@@ -1,28 +1,51 @@
+import { linkWithPopup } from "firebase/auth"
 import { useRouter } from 'next/router'
-import React, { FC, useEffect, useReducer } from 'react'
+import React, { FC, useEffect, useReducer, useState } from 'react'
 import styles from "../styles/Login.module.css"
 import { useAuthUser } from '@/hooks/useAuth'
+import { auth, googleProvider } from "@/lib/firebase"
 import authReducer from '@/reducers/authReducer'
 import { authUseCase } from '@/useCases'
 
 const Login: FC = () => {
-  const router = useRouter();
-  const [, dispatch] = useReducer(authReducer.reducer, authReducer.initialState);
-  const user = useAuthUser();
+  const [isError, setIsError] = useState<boolean>(false)
+  const router = useRouter()
+  const [, dispatch] = useReducer(authReducer.reducer, authReducer.initialState)
+  const user = useAuthUser()
 
   useEffect(() => {
     if (user != null && !user.isAnonymous) {
-      router.push('/');
+      router.push('/')
     }
-  }, [user, router]);
+  }, [user, router])
 
-  const logIn = async () => {
+  const reLogIn = async () => {
     try {
-      await authUseCase.signIn(dispatch);
+      await authUseCase.signIn(dispatch)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
+
+  const loginWithGoogle = () => {
+    if (auth.currentUser) {
+      linkWithPopup(auth.currentUser, googleProvider)
+        .then((result) => {
+          const user = result.user
+          console.log(user)
+          setIsError(false)
+          router.push("/dashboard")
+        })
+        .catch(() => {
+          setIsError(true)
+        })
+    }
+  }
+
+  useEffect(() => {
+    isError && reLogIn()
+  }, [isError])
+
 
   return (
     <main className={`mx-auto my-auto min-h-screen justify-center flex items-center ${styles.container}`}>
