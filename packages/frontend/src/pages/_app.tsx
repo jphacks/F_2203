eb06d96b8257f18753aba8e73a39a1543a5bbcbc
putcho01 <>
2022-10-16 14:38:31 +0900

    ✨ Layout 適用

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppPropsWithLayout } from 'next/app'
 import { AuthProvider } from '@/context/authContext'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </>
-  )
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
 
+  return getLayout(
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>,
+  )
 }
 
 export default MyApp
