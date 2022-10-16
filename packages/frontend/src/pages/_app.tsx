67569096809e402305773178846aa596ffeda1ca
putcho01 <>
2022-10-16 23:10:29 +0900

    Add: QueryClientProvider

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppPropsWithLayout } from 'next/app'
 import { AuthProvider } from '@/context/authContext'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>,
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
