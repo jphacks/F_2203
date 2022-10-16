71f4863289f07954fc2697ea3e6fb8a40934d860
putcho01 <>
2022-10-17 01:02:47 +0900

    Add: Space component

---
+++ packages/frontend/src/components/Space/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface SpacerProps extends React.HtmlHTMLAttributes<HTMLSpanElement> {
+  size: number;
+  axis?: 'vertical' | 'horizontal';
+  style?: Object;
+}
+
+const Spacer = ({ size, axis, style = {}, ...delegated }: SpacerProps) => {
+  const width = axis === 'vertical' ? 0 : size;
+  const height = axis === 'horizontal' ? 0 : size;
+  return (
+    <span
+      style={{
+        display: 'block',
+        width,
+        minWidth: width,
+        height,
+        minHeight: height,
+        ...style,
+      }}
+      {...delegated}
+    />
+  );
+};
+export default Spacer;
