f4410224f697a08e8e595b5da515630f75d9e7d3
putcho01 <>
2022-10-16 20:14:01 +0900

    Fix: typo

--- packages/frontend/src/lib/hasuraClient.ts
+++ packages/frontend/src/lib/hasuraClient.ts
@@ -5,7 +5,7 @@ export const createHasuraClient = (token: string | null) => {
   const headers = {
     'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET ?? ''
   }
-  const client = new GraphQLClient(process.env.NEXT_PUBLIC_ASURA_GRAPHQL_ENDPOINT ?? '', {
+  const client = new GraphQLClient(process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ENDPOINT ?? '', {
     headers,
   })
   return getSdk(client)
