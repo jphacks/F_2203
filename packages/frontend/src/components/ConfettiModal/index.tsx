4be94e3bd5fb6aae5316cc05f63516ff679b2eb8
putcho01 <>
2022-10-17 22:36:29 +0900

    ♻️ リファクタ

--- packages/frontend/src/components/ConfettiModal/index.tsx
+++ packages/frontend/src/components/ConfettiModal/index.tsx
@@ -2,88 +2,85 @@ import { Dialog, Transition } from '@headlessui/react'
 import React, { Fragment, useState } from 'react'
 import Confetti from 'react-confetti'
 import useWindowSize from 'react-use/lib/useWindowSize'
-import styles from "./style.module.css"
+import styles from './style.module.css'
 
 type IConfettiModal = {
-  userName: string,
-  userCustmoId: string,
-  artistName: string,
-  artistImage: string,
+  userName: string
+  userCustmoId: string
+  artistName: string
+  artistImage: string
   isOpen: boolean
   closeModal: () => void
 }
 
-const ConfettiModal: React.FC<IConfettiModal> = ({userName, userCustmoId, artistName, artistImage, isOpen, closeModal}) => {
+const ConfettiModal: React.FC<IConfettiModal> = ({
+  userName,
+  userCustmoId,
+  artistName,
+  artistImage,
+  isOpen,
+  closeModal,
+}) => {
   const { width, height } = useWindowSize()
 
   let hostname = ``
   if (typeof window !== 'undefined') {
-    hostname = window.location.hostname;
- }
+    hostname = window.location.hostname
+  }
 
   return (
     <>
-      {isOpen &&
-        <Confetti
-          width={width}
-          height={height}
-        numberOfPieces={150}
-        gravity={0.05}
-        />
-      }
+      {isOpen && <Confetti width={width} height={height} numberOfPieces={150} gravity={0.05} />}
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as='div' className='relative z-10' onClose={closeModal}>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
               <Transition.Child
                 as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className={styles["animate-charcter"]}
-                  >
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                  <Dialog.Title as='h3' className={styles['animate-charcter']}>
                     Congratulations!!🎉
                   </Dialog.Title>
                   <div className='w-36 rounded mx-auto text-center mt-8'>
                     <img src={artistImage} className={styles.animation_card} />
                   </div>
-                  <div className="mt-4">
-                    <p className="text-sm text-gray-500">
+                  <div className='mt-4'>
+                    <p className='text-sm text-gray-500'>
                       {userName}さんと{artistName}さんとの履歴が追加されました！！やったね！!
                     </p>
                   </div>
 
-                  <div className="mt-4 justify-between flex mx-0">
+                  <div className='mt-4 justify-between flex mx-0'>
                     <a
                       href={`http://twitter.com/share?url=${hostname}/${userCustmoId}&text=${artistName}との初めての履歴を追加したよ！`}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      target='_blank'
+                      rel='noreferrer'
+                      className='rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
                     >
                       Twitterでシェアする
                     </a>
                     <button
-                      type="button"
-                      className="rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-black-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      type='button'
+                      className='rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-black-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
                       onClick={closeModal}
                     >
                       ありがとう。閉じる
--- packages/frontend/src/useCases/postUseCase.ts
+++ packages/frontend/src/useCases/postUseCase.ts
@@ -1,11 +1,51 @@
+import fetcher from '@/lib/fetcher'
+import { createHasuraClient, HasuraClient } from '@/lib/hasuraClient'
+import { GetArtistApiResponse } from '@/pages/api/artists/[ids]'
+
 export default class postUseCase {
+  hasuraClient: HasuraClient
+  constructor() {
+    this.hasuraClient = createHasuraClient(null)
+  }
   /** 投稿 */
-  async postItem() {
+  async insertItem(
+    uid: string,
+    title: string,
+    message: string,
+    locationName: string,
+    locationLat: number,
+    locationLng: number,
+    link: string,
+    spotifyId: string,
+    artistName: string,
+    artistImage: string,
+  ) {
     try {
-      //アーティスト登録
-      //Postデータ登録
+      await this.hasuraClient.UpsertPostWithArtist({
+        uid: uid,
+        title: title,
+        message: message,
+        location_name: locationName,
+        location_lat: locationLat,
+        location_lng: locationLng,
+        link: link,
+        spotify_id: spotifyId,
+        name: artistName,
+        image_url: artistImage,
+      })
     } catch (e) {
-      console.log(e)
+      throw e
+    }
+  }
+
+  /** アーティスト情報取得 */
+  async getArtistInfo(artistId: string) {
+    try {
+      const artistsData: GetArtistApiResponse = await fetcher(`/api/artists/${artistId}`)
+      const artist = artistsData.artists[0]
+      return artist
+    } catch (e) {
+      throw e
     }
   }
 }
