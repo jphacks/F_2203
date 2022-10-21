15746966d9afa03d07fc9eb3e9b76ef9608e8a35
putcho01 <53213591+putcho01@users.noreply.github.com>
2022-10-21 21:27:21 +0900

    Add: メタデータ登録

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppPropsWithLayout } from 'next/app'
+import Head from 'next/head'
 import { AuthProvider } from '@/context/authContext'
 
 const queryClient = new QueryClient()
@@ -10,6 +11,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>オタクの履歴書</title>
+        <meta name='description' content='オタク自ら聖地を作ってこう！' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
     </QueryClientProvider>
   )
