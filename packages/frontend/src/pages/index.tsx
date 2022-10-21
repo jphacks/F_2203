f40bb6ab78c190801790d0f4f2c34c93f04d8cd4
tatara-takumi <tataratakumi@gmail.com>
2022-10-22 00:05:50 +0900

    トップ画面仮

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -3,8 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Layout } from '@/components/Layout'
+import { useEffect, useState } from 'react'
+import { useAuthUser } from '@/hooks/useAuth'
+import Link from 'next/link'
+import Spacer from '@/components/Space'
 
 const Home: NextPageWithLayout = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+  const authUser = useAuthUser()
+  useEffect(() => {
+    if (authUser?.uid) {
+      setIsLoggedIn(true)
+    }
+  }, [authUser])
   return (
     <div className={styles.container}>
       <Head>
@@ -15,37 +26,88 @@ const Home: NextPageWithLayout = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          キャッチコピーを入れる
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
+          オタクの履歴書は、誰でも直感的に使えるサービスです。<br />
+          イベントやライブの思い出を記録して、シェアして、楽しみましょう。
         </p>
 
         <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href='https://github.com/vercel/next.js/tree/canary/examples' className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+          <Image
+            width={800}
+            height={600}
+            objectFit='contain'
+            style={{ width: "auto", maxHeight: "600px", maxWidth: "100%" }}
+            src={"/images/eyecatch.jpg"}
+          ></Image>
         </div>
+
+        <Spacer size={24} />
+        <div className='divider'>
+          {isLoggedIn ?
+            <div className='dropdown dropdown-end'>
+              <Link href='/new'>
+                <a className='text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm sm:w-auto px-5 py-2.5 text-center'>
+                  + New
+                </a>
+              </Link>
+            </div>
+            :
+            <div className='navbar-end mr-4'>
+              <Link href='/login'>
+                <a className='bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm sm:w-auto px-5 py-2.5 text-center text-white'>
+                  ログイン
+                </a>
+              </Link>
+            </div>
+          }
+        </div>
+
+        <div className='text-center mt-8' style={{ width: "60%" }}>
+          <h2 className='font-bold text-2xl'>
+            Features
+          </h2>
+          <div className='flex mx-auto mt-8' >
+            <div className="flex-1" >
+              <Image
+                width={40}
+                height={30}
+                objectFit='contain'
+                style={{ width: "auto", maxHeight: "600px", maxWidth: "100%" }}
+                src={"/images/map-location.svg"}
+              />
+              <p className='font-bold text-sm' >足あとを残す</p>
+              <p className='font-extralight text-xs text-slate-500'>
+                ライブやイベントの足あとを記録しましょう。
+              </p>
+            </div>
+            <div className='flex-1'>
+              <Image
+                width={40}
+                height={30}
+                objectFit='contain'
+                style={{ width: "auto", maxHeight: "600px", maxWidth: "100%" }}
+                src={"/images/music.svg"}
+              />
+              <p className='font-bold text-sm'>アーティストを紐づけ</p>
+              <p className='font-extralight text-xs text-slate-500'>足あとにアーティストを指定すれば、どんなアーティストのイベントに参加したのかが一目で分かります。同じアーティストが好きなユーザーを探すこともできます。</p>
+            </div>
+            <div className='flex-1'>
+              <Image
+                width={40}
+                height={30}
+                objectFit='contain'
+                style={{ width: "auto", maxHeight: "600px", maxWidth: "100%" }}
+                src={"/images/user.svg"}
+              />
+              <p className='font-bold text-sm'>自分だけのプロフィール</p>
+              <p className='font-extralight text-xs text-slate-500' >ライブやイベントを記録すると、プロフィールに足あとが追加されます。足あとの記録が、あなたの魅力を伝えてくれるはずです。</p>
+            </div>
+          </div>
+        </div>
+
       </main>
     </div>
   )
